6fbbb8a29df2fb46a5c7288c9901ba4b18a77d3d
Abdullah Bin Anis <rugusakii@gmail.com>
2024-03-13 22:13:57 +0500

    Image issue

--- src/components/Home/PopularCategories.tsx
+++ src/components/Home/PopularCategories.tsx
@@ -3,7 +3,7 @@ import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import { MoveLeft, MoveRight } from "lucide-react";
 import CategoriesMobCarousel from "./CateogriesMobCarousel";
-import CategoriesImg from "@/assets/images/cat.png"
+import CategoriesImg from "../../assets/images/cat.png"
 
 const PopularCategories = () => {
   const settings = {
